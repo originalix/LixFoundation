2329e03050daef653d800ee4ff772d28f85eab51
Lix <xiao.liunit@gmail.com>
2017-01-16 11:56:15 +0800

    Release 0.0.1

---
+++ Lix_Foundation/LixFoundation.h
@@ -0,0 +1,28 @@
+//
+//  LixFoundation.h
+//  LixTools
+//
+//  Created by Lix on 16/11/29.
+//  Copyright © 2016年 Lix. All rights reserved.
+//
+
+#ifndef LixFoundation_h
+#define LixFoundation_h
+
+#import <UIKit/UIKit.h>
+
+/* 常用Category */
+#import "NSString+LixExtension.h"
+#import "UIBarButtonItem+LixExtension.h"
+#import "UIDevice+LixKeychainIDFV.h"
+#import "UIImage+LixExtension.h"
+#import "UINavigationBar+LixExtension.h"
+#import "UIView+LixExtension.h"
+/* 常用工具类 */
+#import "CalculateLayout.h"
+#import "LixAlertUtil.h"
+#import "LixUtils.h"
+/* 常用Macro */
+#import "LixMacro.h"
+
+#endif /* LixFoundation_h */
